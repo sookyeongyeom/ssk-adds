89834d2a50f907e83a8047ab4b908e6665fffc72
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-14 08:58:46 +0900

    Feat: typographies defined

---
+++ styles/fonts.ts
@@ -0,0 +1,37 @@
+export const Fonts = {
+	/* 300 */
+	light10: 'font-size:1rem; font-weight:300;',
+	light12: 'font-size:1.2rem; font-weight:300;',
+	light14: 'font-size:1.4rem; font-weight:300;',
+	light16: 'font-size:1.6rem; font-weight:300;',
+
+	/* 400 */
+	regular12: 'font-size:1.2rem; font-weight:400;',
+	regular14: 'font-size:1.4rem; font-weight:400;',
+	regular16: 'font-size:1.6rem; font-weight:400;',
+	regular18: 'font-size:1.8rem; font-weight:400;',
+	regular25: 'font-size:2.5rem; font-weight:400;',
+	regular30: 'font-size:3rem; font-weight:400;',
+	regular40: 'font-size:4rem; font-weight:400;',
+
+	/* 500 */
+	medium14: 'font-size:1.4rem; font-weight:500;',
+	medium16: 'font-size:1.6rem; font-weight:500;',
+	medium18: 'font-size:1.8rem; font-weight:500;',
+	medium20: 'font-size:2rem; font-weight:500;',
+	medium22: 'font-size:2.2rem; font-weight:500;',
+	medium24: 'font-size:2.4rem; font-weight:500;',
+	medium25: 'font-size:2.5rem; font-weight:500;',
+
+	/* 700 */
+	bold18: 'font-size:1.8rem; font-weight:700;',
+	bold20: 'font-size:2rem; font-weight:700;',
+	bold23: 'font-size:2.3rem; font-weight:700;',
+	bold25: 'font-size:2.5rem; font-weight:700;',
+	bold28: 'font-size:2.8rem; font-weight:700;',
+	bold30: 'font-size:3rem; font-weight:700;',
+	bold32: 'font-size:3.2rem; font-weight:700;',
+	bold34: 'font-size:3.4rem; font-weight:700;',
+	bold35: 'font-size:3.5rem; font-weight:700;',
+	bold40: 'font-size:4rem; font-weight:700;',
+} as const;
