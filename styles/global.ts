95f6ac98acf6983fcc1ff178ef77e85b612bf82a
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-14 08:59:08 +0900

    Feat: global default font & color defined

--- styles/global.ts
+++ styles/global.ts
@@ -1,20 +1,24 @@
 import { createGlobalStyle } from 'styled-components';
 import { ResetCSS } from './reset';
+import { Colors } from './colors';
 
 export const GlobalStyle = createGlobalStyle`
   ${ResetCSS}
+
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap');
 
   html {
     font-size: 62.5%;
   }
 
   html, body {
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    font-family: 'Noto Sans KR', sans-serif;
   }
 
   * {
-    box-sizing: border-box;
     font-size: 1.6rem;
+    font-weight: 400;
+    box-sizing: border-box;
+    color: ${Colors.gray400};
   }
 `;
