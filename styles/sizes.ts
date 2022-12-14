30a52cbab6d9b449153a5f76944931928c7d5b16
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-14 13:35:39 +0900

    Fix: footer size corrected

--- styles/sizes.ts
+++ styles/sizes.ts
@@ -1,5 +1,5 @@
 export const Sizes = {
 	desktopHeaderHeight: '9.2rem',
-	desktopFooterHeight: '1.21rem',
-	mobileFooterHeight: '2.36rem',
+	desktopFooterHeight: '12.1rem',
+	mobileFooterHeight: '23.9rem',
 } as const;
