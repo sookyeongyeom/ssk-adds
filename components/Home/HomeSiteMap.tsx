65a53fcec941675592455ed401535f1a950ede3c
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-15 03:43:29 +0900

    Feat: home carousel as infinite carousel

--- components/Home/HomeSiteMap.tsx
+++ components/Home/HomeSiteMap.tsx
@@ -1,3 +1,8 @@
+import styled from 'styled-components';
 export default function HomeSiteMap() {
-	return <div>사이트맵</div>;
+	return <S.HomeSiteMapLayout>사이트맵</S.HomeSiteMapLayout>;
+}
+
+namespace S {
+	export const HomeSiteMapLayout = styled.section``;
 }
