a590fcef6419dee0704d879c3b62654b9b3bbb3c
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-14 10:47:57 +0900

    Feat: constants for seo title defined

---
+++ constants/seo.ts
@@ -0,0 +1,16 @@
+export const Seo = {
+	Title: {
+		home: 'Home',
+		contact: 'Contact',
+		faq: 'FAQ',
+		intro: 'ADDS 소개',
+		member: '연구진 소개',
+		news: '보도자료',
+		notice: '공지사항',
+		paper: '데이터활용논문',
+		publication: '발간물',
+		research: '조사설계',
+		resource: '자료안내',
+		login: '로그인',
+	},
+} as const;
