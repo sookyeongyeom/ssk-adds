7aeb382d0cba1d8aedfd8768f73592d9c5606582
sookyeong <sookyeong.dev@gmail.com>
2022-12-14 09:02:06 +0900

    Feature/design definition (#2)
    * Feat: colors defined
    
    * Feat: typographies defined
    
    * Feat: global default font & color defined
    
    * Feat: shadows defined
    
    * Feat: sizes defined
    
    * Feat: svgs defined
    
    * Feat: assets added

---
+++ styles/colors.ts
@@ -0,0 +1,14 @@
+export const Colors = {
+	white: '#FFFFFF',
+
+	gray100: '#EDEDED',
+	gray200: '#C2C2C2',
+	gray300: '#767676',
+	gray400: '#333333',
+
+	blue100: '#E4E9F0',
+	blue200: '#E2EDFB',
+	blue300: '#639AE9',
+	blue400: '#6E98D4',
+	blue500: '#003276',
+} as const;
---
+++ styles/sizes.ts
@@ -0,0 +1,5 @@
+export const Sizes = {
+	desktopHeaderHeight: '9.2rem',
+	desktopFooterHeight: '1.21rem',
+	mobileFooterHeight: '2.36rem',
+} as const;
