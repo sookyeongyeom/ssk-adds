b295e38c58c19ead0aa3258c7e4b7ce773c1474c
sookyeong <sookyeong.dev@gmail.com>
2022-12-16 00:45:04 +0900

    Feature/shared sidebar (#10)
    * Feat: sidebar gets current path as props from layout
    
    * Feat: shared sidebar
    
    * Feat: props & styled types for sidebar
    
    * Feat: link item separated as individual component
    
    * Fix: sitemap menu map key added

--- components/Layout/AddsLayout.tsx
+++ components/Layout/AddsLayout.tsx
@@ -1,10 +1,47 @@
 import Sidebar from '../Shared/Sidebar';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { Seo } from '../../constants/seo';
+import { Fonts } from '../../styles/fonts';
 
 export default function AddsLayout({ children }: ChildrenType) {
+	const router = useRouter();
+	const [title, setTitle] = useState('');
+	const [path, setPath] = useState('');
+
+	useEffect(() => {
+		const path = router.pathname.split('/')[2];
+		setPath('/' + path);
+		setTitle(Seo.Title[path]);
+	}, [router.pathname]);
+
 	return (
-		<>
-			<Sidebar></Sidebar>
-			{children}
-		</>
+		<S.Layout>
+			<Sidebar path={path}></Sidebar>
+			<h1>{title}</h1>
+			<section>{children}</section>
+		</S.Layout>
 	);
+}
+
+namespace S {
+	export const Layout = styled.div`
+		width: fit-content;
+		margin: 0 auto;
+		display: grid;
+		grid-template-columns: 17rem 99.5rem;
+		column-gap: 6.6rem;
+		row-gap: 3.8rem;
+		padding-top: 3.9rem;
+
+		> aside {
+			grid-row: 2/3;
+		}
+
+		> h1 {
+			${Fonts.regular40}
+			grid-column: 2/3;
+		}
+	`;
 }
