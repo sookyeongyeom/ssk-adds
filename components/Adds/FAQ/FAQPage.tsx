75c6f64b0266013aeffe81347988fceb68f6bf2c
sookyeong <sookyeong.dev@gmail.com>
2022-12-16 05:26:18 +0900

    Feat: show dummy data if api failed (#15)

--- components/Adds/FAQ/FAQPage.tsx
+++ components/Adds/FAQ/FAQPage.tsx
@@ -18,7 +18,7 @@ export default function FAQPage() {
 
 	return (
 		<S.FAQPageLayout>
-			{faqs &&
+			{faqs ? (
 				faqs.items.map((faq, i) => (
 					<FAQBoxElement
 						idx={faq.id + 1}
@@ -27,7 +27,19 @@ export default function FAQPage() {
 						answer={faq.reply}
 						key={i}
 					/>
-				))}
+				))
+			) : (
+				<FAQBoxElement
+					idx={0 + 1}
+					category={'OOOOOO프로젝트'}
+					question={
+						'OOOOOO프로젝트에 참여하는 사람입니다.\n하루에 세 번 OOOO을 하라고 안내 받았는데, 놓친 날이 있다면 어떻게 해야할까요?'
+					}
+					answer={
+						'OOOOOO프로젝트에 참여하시는 분들은 하루에 세 번 OOOO을 하시기를 권장드립니다.\n그러나 놓친 날이 있는 경우엔 데일리 설문조사에서 놓친 날짜, 시간에 대한 정보를 꼭 기입해주시기 바랍니다.\n혹시 설문조사에서 쓰지 않은 경우엔 reaiqwhl@yonsei.ac.kr로 연락 주세요.'
+					}
+				/>
+			)}
 		</S.FAQPageLayout>
 	);
 }
