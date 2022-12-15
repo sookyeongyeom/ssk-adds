f42f1518acd46e1e609c88bc27c956252b98f1db
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-16 05:55:17 +0900

    Feat: import fonts at _document not globalStyle

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,7 +29,15 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html>
-				<Head />
+				<Head>
+					{/* Noto Sans */}
+					<link rel='preconnect' href='https://fonts.googleapis.com' />
+					<link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
+					<link
+						href='https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap'
+						rel='stylesheet'
+					/>
+				</Head>
 				<body>
 					<Main />
 					<NextScript />
