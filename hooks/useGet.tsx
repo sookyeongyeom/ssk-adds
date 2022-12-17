ca3c0d85b3118aa1a7066e48ef6b5cf35ab355f3
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-17 19:54:34 +0900

    Feat: useGet hook to check if api & dispatch types are corresponding

--- hooks/useGet.tsx
+++ hooks/useGet.tsx
@@ -1,4 +1,9 @@
-export default async function useGet(api: any, dispatch: any) {
+import { Dispatch, SetStateAction } from 'react';
+
+export default async function useGet<T>(
+	api: () => Promise<T>,
+	dispatch: Dispatch<SetStateAction<T | undefined>>,
+) {
 	const res = await api();
 	dispatch(res);
 }
