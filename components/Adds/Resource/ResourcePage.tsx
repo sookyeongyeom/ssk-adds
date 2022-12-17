1f22468395668f70b2a72a720ae0131f81799b4f
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-17 20:40:07 +0900

    Feat: apply api to adds resource

--- components/Adds/Resource/ResourcePage.tsx
+++ components/Adds/Resource/ResourcePage.tsx
@@ -1,3 +1,24 @@
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { ResponseResource } from '../../../@types/api/resource';
+import useGet from '../../../hooks/useGet';
+import Board from '../../Shared/Board';
+import { getResource } from '../../../api/resource';
+
 export default function ResourcePage() {
-	return <>자료</>;
+	const [resource, setResource] = useState<ResponseResource.Get>();
+
+	useEffect(() => {
+		useGet(() => getResource({ page: 1 }), setResource);
+	}, []);
+
+	return (
+		<S.ResourcePageLayout>
+			<Board datas={resource} />
+		</S.ResourcePageLayout>
+	);
+}
+
+namespace S {
+	export const ResourcePageLayout = styled.div``;
 }
