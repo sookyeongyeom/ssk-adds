f9f622344271276ec8ee28b24bc199b9b01bb7f0
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-18 00:57:45 +0900

    Feat: adds notice & notice view

--- components/Adds/Notice/NoticePage.tsx
+++ components/Adds/Notice/NoticePage.tsx
@@ -1,3 +1,24 @@
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import useGet from '../../../hooks/useGet';
+import Board from '../../Shared/Board';
+import { ResponseNotice } from '../../../@types/api/notice';
+import { getNotice } from '../../../api/notice';
+
 export default function NoticePage() {
-	return <>공지사항</>;
+	const [notice, setNotice] = useState<ResponseNotice.Get>();
+
+	useEffect(() => {
+		useGet(() => getNotice({ page: 1 }), setNotice);
+	}, []);
+
+	return (
+		<S.NoticePageLayout>
+			<Board datas={notice!} isNotice />
+		</S.NoticePageLayout>
+	);
+}
+
+namespace S {
+	export const NoticePageLayout = styled.div``;
 }
--- components/Shared/View.tsx
+++ components/Shared/View.tsx
@@ -6,25 +6,27 @@ import { Colors } from '../../styles/colors';
 import { svgDownload } from '../../styles/svgs';
 import BoardButton from './BoardButton';
 import AdjacentNavigator from './AdjacentNavigator';
+import { ResponseNotice } from '../../@types/api/notice';
 
-export default function View<T extends ResponseResource.GetById>({
+export default function View<T extends ResponseResource.GetById | ResponseNotice.GetById>({
 	data,
 	boardPath,
 	prev,
 	next,
+	isNotice = false,
 }: ViewProps<T>) {
 	return (
 		<S.ViewLayout>
 			{data && (
 				<>
-					<S.Meta>
+					<S.Meta isNotice={isNotice}>
 						<h1>{data.title}</h1>
 						<h2>
 							{data.id}&ensp;|&ensp;{data.writer}&ensp;|&ensp;{data.created_date}
 						</h2>
 					</S.Meta>
 					<S.Content dangerouslySetInnerHTML={{ __html: data.body }} />
-					<S.File>
+					<S.File isNotice={isNotice}>
 						<h3>첨부파일</h3>
 						{data.file && (
 							<p>
@@ -43,9 +45,11 @@ export default function View<T extends ResponseResource.GetById>({
 namespace S {
 	export const ViewLayout = styled.div``;
 
-	export const Meta = styled.div`
+	export const Meta = styled.div<ViewLayoutProps>`
 		padding: 2.5rem 1.3rem;
-		border-bottom: 0.1rem solid ${Colors.gray150};
+		padding-top: 3.5rem;
+		border-bottom: ${(props) => (props.isNotice ? '' : `0.1rem solid ${Colors.gray150}`)};
+		background-color: ${(props) => (props.isNotice ? Colors.blue100 : '')};
 
 		> h1 {
 			${Fonts.medium24}
@@ -62,8 +66,10 @@ namespace S {
 		padding: 5.8rem 3rem;
 	`;
 
-	export const File = styled.div`
-		background-color: ${Colors.blue100};
+	export const File = styled.div<ViewLayoutProps>`
+		background-color: ${(props) => (props.isNotice ? '' : Colors.blue100)};
+		border-top: ${(props) => (props.isNotice ? `0.1rem solid ${Colors.gray150}` : '')};
+		border-bottom: ${(props) => (props.isNotice ? `0.1rem solid ${Colors.gray150}` : '')};
 		height: 5.5rem;
 		padding: 0 3rem;
 		display: flex;
