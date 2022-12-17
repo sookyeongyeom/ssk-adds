df40bc301498dc4c8c5bb5b4f9c75642644131e9
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-18 03:16:48 +0900

    Feat: recipe to align select box top right

---
+++ styles/recipes.ts
@@ -0,0 +1,10 @@
+import { css } from 'styled-components';
+
+export namespace Recipes {
+	export const AlignSelectBoxForBoard = css`
+		> div:first-of-type {
+			margin-left: auto;
+			margin-bottom: 1rem;
+		}
+	`;
+}
