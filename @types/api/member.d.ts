a30e874416fb547cf25fe4c4975554f381503e60
sookyeong <sookyeong.dev@gmail.com>
2022-12-17 19:58:21 +0900

    Feature/adds member api (#20)
    * Feat: types for member get
    
    * Feat: member api get
    
    * Feat: useGet hook to check if api & dispatch types are corresponding
    
    * Fix: apply request type to faq api params
    
    * Feat: apply api to adds member

---
+++ @types/api/member.d.ts
@@ -0,0 +1,27 @@
+export namespace RequestMember {
+	type Get = {
+		page?: number;
+		size?: number;
+	};
+}
+
+export namespace ResponseMember {
+	type Get = {
+		items: [
+			{
+				id: number;
+				name: string;
+				email: string;
+				homepage: string;
+				phone_number: string;
+				intro_body: string;
+				job_title: string;
+				img: string;
+				responsibility: string;
+			},
+		];
+		total: number;
+		page: number;
+		size: number;
+	};
+}
--- api/faq.ts
+++ api/faq.ts
@@ -1,7 +1,7 @@
-import { RequestFAQ } from '../@types/api/faq';
+import { RequestFAQ, ResponseFAQ } from '../@types/api/faq';
 import request from './core';
 
-export const getFAQ = (page: number, size: number) => {
+export const getFAQ = ({ page, size }: RequestFAQ.Get) => {
 	const url = `/community/faq?page=${page}&size=${size}`;
-	return request.get<RequestFAQ.Get>(url);
+	return request.get<ResponseFAQ.Get>(url);
 };
