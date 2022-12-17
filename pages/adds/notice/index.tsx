fdf337e6f391a6be108f698d878308c5bed656b2
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-18 00:55:59 +0900

    Feat: dynamic route for notice view

--- pages/adds/notice/index.tsx
+++ pages/adds/notice/index.tsx
@@ -1,9 +1,9 @@
 import { ReactElement } from 'react';
-import NoticePage from '../../components/Adds/Notice/NoticePage';
-import AddsLayout from '../../components/Layout/AddsLayout';
-import HomeLayout from '../../components/Layout/HomeLayout';
-import CustomHead from '../../components/Seo/CustomHead';
-import { Seo } from '../../constants/seo';
+import AddsLayout from '../../../components/Layout/AddsLayout';
+import HomeLayout from '../../../components/Layout/HomeLayout';
+import CustomHead from '../../../components/Seo/CustomHead';
+import { Seo } from '../../../constants/seo';
+import NoticePage from '../../../components/Adds/Notice/NoticePage';
 
 export default function notice() {
 	return (
