a4f249fc05affdd44c3dea97aa936b90462ec3a3
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-17 19:02:37 +0900

    Feat: font medium12 added

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -15,6 +15,7 @@ export const Fonts = {
 	regular40: 'font-size:4rem; font-weight:400;',
 
 	/* 500 */
+	medium12: 'font-size:1.2rem; font-weight:500;',
 	medium14: 'font-size:1.4rem; font-weight:500;',
 	medium16: 'font-size:1.6rem; font-weight:500;',
 	medium18: 'font-size:1.8rem; font-weight:500;',
