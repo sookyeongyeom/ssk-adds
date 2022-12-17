89b4da3408dc7ac1c615978cac39cac55a501a2a
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-18 02:30:32 +0900

    Fix: header min width equals to adds layout width

--- styles/sizes.ts
+++ styles/sizes.ts
@@ -2,6 +2,7 @@ export const Sizes = {
 	desktopHeaderHeight: '9.2rem',
 	desktopFooterHeight: '12.1rem',
 	desktopHomeWidth: '120rem',
+	desktopAddsWidth: '123.1rem',
 	desktopHomeSliderHeight: '48rem',
 	desktopCarouselItemWidth: '30.7rem',
 	desktopCarouselItemHeight: '9.1rem',
