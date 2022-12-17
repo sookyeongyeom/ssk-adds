e8bfdc1791eefeb2f3ef4b168e7af035800ae9fb
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-17 16:43:28 +0900

    Feat: adds research

--- components/Adds/Research/ResearchPage.tsx
+++ components/Adds/Research/ResearchPage.tsx
@@ -1,3 +1,111 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import { Fonts } from '../../../styles/fonts';
+import { Colors } from '../../../styles/colors';
+import { BoxShadows } from '../../../styles/shadows';
+
 export default function ResearchPage() {
-	return <>리서치</>;
+	return (
+		<S.ResearchPageLayout>
+			<ResearchBoxElement
+				title={'기초 설문조사 (ADDS-C; Core)'}
+				description={
+					'알파 세대의 사회정서 발달(기질, 우울, 불안 등), 가족 내 상호작용, 학교 적응에 대한 양적 설문조사\n\n알파 세대 400명 대상 1~3차년도 연차별 1회 반복 수집'
+				}
+			/>
+			<ResearchBoxElement
+				title={'알파 세대 디지털 일상 데이터\n(ADDS-D; Daily)'}
+				description={
+					'알파 세대의 사회정서 발달(기질, 우울, 불안 등), 가족 내 상호작용, 학교 적응에 대한 양적 설문조사\n\n알파 세대 400명 대상 1~3차년도 연차별 1회 반복 수집'
+				}
+			/>
+			<ResearchBoxElement
+				title={'알파 세대의 사회화 과정에 관한 질적 데이터\n(ADDS-Q; Qualitative)'}
+				description={
+					'알파 세대의 부모(1차년도), 교사(2차년도), 알파 세대 본인(3차년도) 대상 심층 인터뷰\n\n알파 세대 청소년과 주요 체계를 대상으로 디지털 일상과 사회화 과정 및 특성, 새로운 이슈에 대한 심층 인터뷰'
+				}
+			/>
+			<ResearchBoxElement
+				title={'알파 세대 일상에 대한 스마트 디바이스 측정\n(ADDS-S; Smart)'}
+				description={
+					'스마트 디바이스(스마트 워치)의 전기반응 센서와 행동기반 기술을 활용하여 알파 세대의 운동, 식사, 수면, 디지털 사용을 추적\n\n2차와 3차년도에 200명을 대상으로 1일 3회(아침,점심,저녁) 1주일간 측정'
+				}
+			/>
+			<ResearchBoxElement
+				title={'생체 지표 (ADDS-B; Bio)'}
+				description={
+					'(타액) 타액(Salivary) 코티졸, DHEA(Dehydroepiandrosterone), ACTH(adrenocorticotropic), 카테콜아민(Catecholamine) 등의 지표를 통해 스트레스에 대한 생리적 반응 측정\n\n(전기 반응 센서) EMG(Electro–myography), BIA(Bio-Impedence Analysis), DOMA(Detection Of Muscle Activation), ECG(Electro- cardiogram)\n\n1차년도 프로토콜 세팅, 2차년도 20명 대상 파일럿 테스트 후, 3차년도 200명 대상 생체지표 측정'
+				}
+			/>
+		</S.ResearchPageLayout>
+	);
+}
+
+function ResearchBoxElement({ title, description }: ResearchBoxElementProps) {
+	const [isHover, setIsHover] = useState(false);
+
+	const onMouseEnter = () => setIsHover(true);
+	const onMouseLeave = () => setIsHover(false);
+
+	return (
+		<S.ResearchBox onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} isHover={isHover}>
+			<S.Title as={!isHover ? 'pre' : 'h2'} isHover={isHover}>
+				{title}
+			</S.Title>
+			{isHover && <S.Description>{description}</S.Description>}
+			{!isHover && <S.Fold />}
+		</S.ResearchBox>
+	);
+}
+
+namespace S {
+	export const ResearchPageLayout = styled.div`
+		display: flex;
+		flex-direction: column;
+		gap: 3.1rem;
+
+		/* 생체지표 */
+		> div:last-of-type {
+			height: 27.4rem;
+			gap: 2.5rem;
+			line-height: 120%;
+		}
+	`;
+
+	export const ResearchBox = styled.div<ResearchBoxProps>`
+		width: 100%;
+		height: 21.1rem;
+		background-image: ${(props) =>
+			!props.isHover ? 'url(/assets/pattern_white.png)' : 'url(/assets/pattern_blue.png)'};
+		background-repeat: no-repeat;
+		background-size: cover;
+		transition: 0.3s ease;
+		transition-property: background;
+		display: flex;
+		flex-direction: column;
+		gap: 3rem;
+		justify-content: ${(props) => (!props.isHover ? 'center' : 'flex-start')};
+		padding: ${(props) => (!props.isHover ? '7rem' : '4.3rem 4.8rem')};
+		position: relative;
+		box-shadow: ${BoxShadows.smooth};
+	`;
+
+	export const Title = styled.h2<ResearchBoxProps>`
+		${(props) => (!props.isHover ? Fonts.bold28 : Fonts.bold20)}
+		color: ${(props) => props.isHover && Colors.blue100};
+		line-height: 130%;
+	`;
+
+	export const Description = styled.pre`
+		${Fonts.medium16}
+		padding: 0 3rem;
+	`;
+
+	export const Fold = styled.div`
+		border-top: 7.2rem solid ${Colors.blue200};
+		border-right: 7.2rem solid ${Colors.blue300};
+		position: absolute;
+		bottom: 0;
+		right: 0;
+	`;
 }
