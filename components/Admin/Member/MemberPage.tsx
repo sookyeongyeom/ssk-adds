9fe48063c481752175e327fcecefbf84f8fe0e34
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-19 06:40:50 +0900

    Feat: admin member board

--- components/Admin/Member/MemberPage.tsx
+++ components/Admin/Member/MemberPage.tsx
@@ -4,15 +4,17 @@ import useGet from '../../../hooks/useGet';
 import { getMember } from '../../../api/member';
 import { ResponseMember } from '../../../@types/api/member';
 import useBoard from '../../../hooks/useBoard';
+import { Paths } from '../../../constants/paths';
+import { ResponseMemberKeys } from '../../../constants/responseKeys';
 
 export default function MemberPage() {
 	const [maps, setMaps] = useState<Map<string, string>[]>();
 	const [member, setMember] = useState<ResponseMember.Get>();
 
 	const order = new Map() //
-		.set('번호', 'id')
-		.set('이름', 'name')
-		.set('역할', 'responsibility');
+		.set(ResponseMemberKeys.id, '번호')
+		.set(ResponseMemberKeys.name, '이름')
+		.set(ResponseMemberKeys.responsibility, '역할');
 
 	useEffect(() => {
 		useGet(() => getMember({}), setMember);
@@ -23,7 +25,7 @@ export default function MemberPage() {
 	return (
 		<>
 			<h1>Admin 연구진소개</h1>
-			<AdminBoard dataMaps={maps} />
+			<AdminBoard dataMaps={maps} basePath={Paths.admin + Paths.member} order={order} />
 		</>
 	);
 }
