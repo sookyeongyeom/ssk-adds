a927c25588b27eaa4b660d667e566fc956856235
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-19 10:59:53 +0900

    Feat: admin resource new

--- components/Admin/Resource/ResourceNewPage.tsx
+++ components/Admin/Resource/ResourceNewPage.tsx
@@ -1,3 +1,39 @@
+import dynamic from 'next/dynamic';
+import { useState } from 'react';
+import AdminButton from '../../Shared/AdminButton';
+import Input from '../../Shared/Input';
+import useInput from '../../../hooks/useInput';
+import { postResource } from '../../../api/resource';
+
+const Editor = dynamic(() => import('../../Shared/Editor'), {
+	ssr: false,
+});
+
 export default function ResourceNewPage() {
-	return <>새로운자료안내</>;
+	const { value: title, onChange: onChangeTitle } = useInput();
+	const { value: writer, onChange: onChangeWriter } = useInput();
+	const [body, setBody] = useState('');
+
+	const onChange = (text: string) => setBody(text);
+
+	const onSubmit = async () => {
+		const res = await postResource({
+			writer,
+			title,
+			body: body,
+			file: '',
+			created_date: new Date().toISOString().split('T')[0],
+		});
+		console.log(res);
+	};
+
+	return (
+		<>
+			새로운자료안내
+			<Input label={'제목'} value={title} onChange={onChangeTitle} />
+			<Editor value={body} onChange={onChange} />
+			<Input label={'관리자'} value={writer} onChange={onChangeWriter} />
+			<AdminButton onClick={onSubmit}>완료</AdminButton>
+		</>
+	);
 }
