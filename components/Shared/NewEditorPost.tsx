5bda07e4345b17d136966142c2b89b897d50f5dc
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-20 02:34:24 +0900

    Feat: file upload ui logic

--- components/Shared/NewEditorPost.tsx
+++ components/Shared/NewEditorPost.tsx
@@ -11,15 +11,26 @@ export default function NewEditorPost({
 	title,
 	writer,
 	body,
+	files,
+	fileRef,
 	onChangeTitle,
 	onChangeWriter,
 	onChangeBody,
+	onAddFile,
+	onRemoveFile,
 	onSubmit,
 }: NewEditorPostProps) {
 	return (
 		<>
 			<Input label={'제목'} value={title} onChange={onChangeTitle} />
 			<Editor value={body} onChange={onChangeBody} />
+			<Input label={'첨부파일'} type={'file'} inputRef={fileRef} onChange={onAddFile} />
+			{files.map((file, i) => (
+				<div key={i}>
+					{file.name}
+					<AdminButton onClick={() => onRemoveFile(file.lastModified)}>X</AdminButton>
+				</div>
+			))}
 			<Input label={'관리자'} value={writer} onChange={onChangeWriter} />
 			<AdminButton onClick={onSubmit}>완료</AdminButton>
 		</>
