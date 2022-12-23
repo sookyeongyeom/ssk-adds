386b4efbd0d49942f7342e5172b150f2cf0433ce
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-23 10:38:16 +0900

    Feat: api for member post

--- api/member.ts
+++ api/member.ts
@@ -1,3 +1,4 @@
+import { toSnake } from 'snake-camel';
 import { RequestMember, ResponseMember } from '../@types/api/member';
 import request from './core';
 
@@ -6,4 +7,9 @@ const baseUrl = '/intro/researchers';
 export const getMember = ({ page }: RequestMember.Get) => {
 	const url = `${baseUrl}?page=${page}`;
 	return request.get<ResponseMember.Get>(url);
+};
+
+export const postMember = (data: RequestMember.Post) => {
+	const url = `${baseUrl}`;
+	return request.post<ResponseMember.Post, RequestMember.Post>(url, toSnake({ id: 0, ...data }));
 };
