064fc1a3c204eff45b78db2ca7bea46029150a99
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-23 16:54:37 +0900

    Feat: paper new

--- components/Admin/Paper/PaperNewPage.tsx
+++ components/Admin/Paper/PaperNewPage.tsx
@@ -1,3 +1,36 @@
+import useInput from '../../../hooks/useInput';
+import { postPaper } from '../../../api/paper';
+import Input from '../../Element/Shared/Input';
+import AdminButton from '../../Element/Admin/AdminButton';
+
 export default function PaperNewPage() {
-	return <>논문 뉴</>;
+	const { value: title, onChange: onChangeTitle } = useInput();
+	const { value: year, onChange: onChangeYear } = useInput();
+	const { value: keywords, onChange: onChangeKeywords } = useInput();
+	const { value: researcherName, onChange: onChangeResearcherName } = useInput();
+	const { value: doi, onChange: onChangeDoi } = useInput();
+
+	const onSubmit = async () => {
+		/* POST */
+		const res = await postPaper({
+			title,
+			year,
+			keywords,
+			researcherName,
+			doi,
+		});
+		console.log(res);
+	};
+
+	return (
+		<>
+			새로운데이터활용논문
+			<Input label={'제목'} onChange={onChangeTitle} />
+			<Input label={'작성연도'} onChange={onChangeYear} />
+			<Input label={'키워드'} onChange={onChangeKeywords} />
+			<Input label={'연구자'} onChange={onChangeResearcherName} />
+			<Input label={'DOI'} onChange={onChangeDoi} />
+			<AdminButton onClick={onSubmit}>완료</AdminButton>
+		</>
+	);
 }
