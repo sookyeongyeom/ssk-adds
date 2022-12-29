6dbc289b2efa466974adb1d2f4c616b80b47fbb6
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-29 11:23:15 +0900

    Feat: faq view design

--- components/Admin/FAQ/FAQViewPage.tsx
+++ components/Admin/FAQ/FAQViewPage.tsx
@@ -4,9 +4,8 @@ import { ViewPageProps } from '../../../@types/pages';
 import { Paths } from '../../../constants/paths';
 import useEditDelete from '../../../hooks/useEditDelete';
 import useGet from '../../../hooks/useGet';
-import AdminButton from '../../Element/Admin/AdminButton';
 import { getFAQById } from '../../../api/faq';
-import AdminBoardButton from '../../Element/Shared/AdminBoardButton';
+import AdminView from '../../Element/Admin/AdminView';
 
 export default function FAQViewPage({ id }: ViewPageProps) {
 	const basePath = Paths.admin + Paths.faq;
@@ -19,17 +18,16 @@ export default function FAQViewPage({ id }: ViewPageProps) {
 
 	return (
 		<div>
-			<AdminButton onClick={onEdit}>수정</AdminButton>{' '}
-			<AdminButton onClick={onDelete}>삭제</AdminButton>
-			{faq && (
-				<>
-					<div>제목:{faq.title}</div>
-					<div>작성자:{faq.writer}</div>
-					<div>분류:{faq.category}</div>
-					<div>답변:{faq.reply}</div>
-				</>
-			)}
-			<AdminBoardButton boardPath={basePath} />
+			<AdminView id={id} basePath={basePath} onEdit={onEdit} onDelete={onDelete}>
+				<div>제목</div>
+				<div>{faq?.title}</div>
+				<div>분류</div>
+				<div>{faq?.category}</div>
+				<div>작성자</div>
+				<div>{faq?.writer}</div>
+				<div>답변</div>
+				<div>{faq?.reply}</div>
+			</AdminView>
 		</div>
 	);
 }
