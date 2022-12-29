77f9fdea20597f544d3a83e9a5f69c0fe8793eb9
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-29 16:35:52 +0900

    Feat: member new design

--- components/Admin/Member/MemberNewPage.tsx
+++ components/Admin/Member/MemberNewPage.tsx
@@ -2,12 +2,11 @@ import useInput from '../../../hooks/useInput';
 import useFiles from '../../../hooks/useFiles';
 import { S3Folders } from '../../../constants/s3';
 import { postMember } from '../../../api/member';
-import Input from '../../Element/Shared/Input';
-import AdminButton from '../../Element/Admin/AdminButton';
 import FileUploadElement from '../../Element/Admin/FileUploadElement';
 import ImagePreview from '../../Element/Admin/ImagePreview';
 import useRoute from '../../../hooks/useRoute';
 import { Paths } from '../../../constants/paths';
+import MemberNewEdit from './MemberNewEdit';
 
 export default function MemberNewPage() {
 	const { value: name, onChange: onChangeName } = useInput();
@@ -49,16 +48,26 @@ export default function MemberNewPage() {
 
 	return (
 		<>
-			<Input label={'이름'} onChange={onChangeName} />
-			<Input label={'이메일'} onChange={onChangeEmail} />
-			<Input label={'홈페이지'} onChange={onChangeHomepage} />
-			<Input label={'연락처'} onChange={onChangePhoneNumber} />
-			<Input label={'소개'} onChange={onChangeIntroBody} />
-			<Input label={'직무'} onChange={onChangeJobTitle} />
-			<Input label={'역할'} onChange={onChangeResponsibility} />
-			<ImagePreview file={files[0]} />
-			<FileUploadElement files={files} onAddFile={onAddFile} onRemoveFile={onRemoveFile} />
-			<AdminButton onClick={onSubmit}>완료</AdminButton>
+			<MemberNewEdit
+				name={name}
+				email={email}
+				homepage={homepage}
+				phoneNumber={phoneNumber}
+				introBody={introBody}
+				jobTitle={jobTitle}
+				responsibility={responsibility}
+				onChangeName={onChangeName}
+				onChangeEmail={onChangeEmail}
+				onChangeHomepage={onChangeHomepage}
+				onChangePhoneNumber={onChangePhoneNumber}
+				onChangeIntroBody={onChangeIntroBody}
+				onChangeJobTitle={onChangeJobTitle}
+				onChangeResponsibility={onChangeResponsibility}
+				onSubmit={onSubmit}>
+				<div>사진</div>
+				<ImagePreview file={files[0]} />
+				<FileUploadElement files={files} onAddFile={onAddFile} onRemoveFile={onRemoveFile} />
+			</MemberNewEdit>
 		</>
 	);
 }
