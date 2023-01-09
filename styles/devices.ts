86435dc5859e10b9c458639e121158605225df64
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-10 07:03:49 +0900

    Feat: breakPointsInNumber as constant

--- styles/devices.ts
+++ styles/devices.ts
@@ -1,8 +1,12 @@
-export const BreakPoints = {
+const BreakPoints = {
 	general: '1200px',
 } as const;
 
 export const Devices = {
 	mobile: `(max-width: ${BreakPoints.general})`,
 	desktop: `(min-width: ${BreakPoints.general})`,
+} as const;
+
+export const BreakPointsInNumber = {
+	general: +BreakPoints.general.replace(/[a-z]/g, ''),
 } as const;
