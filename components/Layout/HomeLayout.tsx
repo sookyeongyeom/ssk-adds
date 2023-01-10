f6b182b475f191bdc3ba5499a87546f87a29956e
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-11 04:06:27 +0900

    Feat: drawer control at home layout

--- components/Layout/HomeLayout.tsx
+++ components/Layout/HomeLayout.tsx
@@ -3,11 +3,41 @@ import Footer from '../Element/Adds/Footer';
 import Header from '../Element/Adds/Header';
 import { Devices } from '../../styles/devices';
 import { Sizes } from '../../styles/sizes';
+import Drawer from '../Element/Adds/Drawer';
+import { useState, useEffect, useRef, MutableRefObject } from 'react';
+import useTitlePath from '../../hooks/useTitlePath';
 
 export default function HomeLayout({ children }: ChildrenType) {
+	const { path } = useTitlePath();
+	const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+	const drawerRef = useRef() as MutableRefObject<HTMLDivElement>;
+
+	const onOpenDrawer = (e: React.MouseEvent) => {
+		e.stopPropagation();
+		setIsDrawerOpen(true);
+	};
+
+	const onCloseDrawer = () => setIsDrawerOpen(false);
+
+	const onClickOutside = (e: CustomEvent<MouseEvent>) => {
+		if (!drawerRef?.current?.contains(e.target as Node)) {
+			onCloseDrawer();
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener('click', onClickOutside as EventListener);
+		return () => window.removeEventListener('click', onClickOutside as EventListener);
+	}, []);
+
+	useEffect(() => {
+		onCloseDrawer();
+	}, [path]);
+
 	return (
 		<S.Layout>
-			<Header />
+			<Header onOpenDrawer={onOpenDrawer} />
+			<Drawer isDrawerOpen={isDrawerOpen} drawerRef={drawerRef} />
 			<main>{children}</main>
 			<Footer />
 		</S.Layout>
